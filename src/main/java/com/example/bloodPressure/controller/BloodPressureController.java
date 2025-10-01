82a5c1158a125e074ecf5bcc7063b2dce66cab7c
Adithyan <adithyanprabhakaran.ie@gmail.com>
2025-10-01 19:31:20 +0100

    add workflow to checkout the code and build

--- src/main/java/com/example/bloodPressure/controller/BloodPressureController.java
+++ src/main/java/com/example/bloodPressure/controller/BloodPressureController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-//@RequestMapping(value = "/api/v1")
 public class BloodPressureController {
 
     @Autowired
