f713d1a5bc1cd325671f25d5fd6a0d7b28761218
Adithyan <adithyanprabhakaran.ie@gmail.com>
2025-11-20 13:51:09 +0000

    fix penetration test

--- zap-scripts/post-api.py
+++ zap-scripts/post-api.py
@@ -1,10 +1,14 @@
 def zap_started(zap, target):
     zap.urlopen(target)
-    zap.core.send_request(
-        {
-            "method": "POST",
-            "url": f"{target}/bp",
-            "body": '{"systolic":120,"diastolic":80}',
-            "headers": {"Content-Type": "application/json"}
-        }
+
+    raw_request = (
+        "POST /bp HTTP/1.1\r\n"
+        f"Host: {target.replace('http://', '').replace('https://', '')}\r\n"
+        "Content-Type: application/json\r\n"
+        "Connection: close\r\n"
+        "Content-Length: 33\r\n"
+        "\r\n"
+        '{"systolic":120,"diastolic":80}'
     )
+
+    zap.core.send_request(raw_request)
