17d9567700a7f17d027f3aa108fe220338f3b13f
Adithyan <adithyanprabhakaran.ie@gmail.com>
2025-12-14 03:14:22 +0000

    release latest to prod

--- src/main/java/com/example/bloodPressure/config/BeanConfig.java
+++ src/main/java/com/example/bloodPressure/config/BeanConfig.java
@@ -3,11 +3,13 @@ package com.example.bloodPressure.config;
 import com.itextpdf.text.Document;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class BeanConfig {
 
     @Bean
+    @Scope("prototype")
     public Document getDocument(){
         return new Document();
     }
